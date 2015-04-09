8d41e8eb40fa2bf0af160e5f045875122c5b5838
Alexey Ganzha <aganzha@yandex.ru>
2015-04-09 11:33:30 +0300

    redeploy admin role

--- CoditCMS/KonigLabs/Migrations/201503211106348_AdminRole.cs
+++ CoditCMS/KonigLabs/Migrations/201503211106348_AdminRole.cs
@@ -16,19 +16,19 @@ namespace KonigLabs.Migrations
             
             using (var context = ApplicationDbContext.Create())
             {
-                ApplicationUserManager userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-                var roleResult = roleManager.Create(new IdentityRole() { Name = CMS.PagesSettings.Settings.AdminRole });
-                Console.WriteLine(roleResult.Succeeded.ToString());
+                //ApplicationUserManager userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+                //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+                //var roleResult = roleManager.Create(new IdentityRole() { Name = CMS.PagesSettings.Settings.AdminRole });
+                //Console.WriteLine(roleResult.Succeeded.ToString());
 
-                var user = new ApplicationUser { Email = username, UserName = username };
-                var userResult = userManager.Create(user, password);
+                //var user = new ApplicationUser { Email = username, UserName = username };
+                //var userResult = userManager.Create(user, password);
 
-                Console.WriteLine(userResult.Succeeded.ToString());
-                if (userResult.Succeeded)
-                {
-                    userManager.AddToRole(user.Id, "admin");
-                }
+                //Console.WriteLine(userResult.Succeeded.ToString());
+                //if (userResult.Succeeded)
+                //{
+                //    userManager.AddToRole(user.Id, "admin");
+                //}
             }
         }
 
